2fdd9f8c1584fccda57fca6d43beff281748356d
jjsarmiento <sarmiento11102@gmail.com>
2015-12-05 11:25:01 +0800

    first push

---
+++ application/views/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<!-- STYLES - START -->
+		<!-- STYLES - END -->
+	
+		<!-- SCRIPTS - START -->
+		<script type="text/javascript" src="<?=base_url()?>assets/js/jquery.min.js"></script>
+		<!-- SCRIPTS - END -->
+	</head>
+	<body>
+	</body>
+</html>
