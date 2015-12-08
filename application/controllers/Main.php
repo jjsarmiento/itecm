291e28da6cb352805f6b2481b0bd8f9852417403
jjsarmiento <sarmiento11102@gmail.com>
2015-12-08 13:44:09 +0800

    assets and general layout

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,6 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 	public function index(){
-		$this->load->view('index');
-	}
+        $data['title'] = 'Discipulus Bookshop';
+
+		$this->load->view('header and footer/header', $data);
+        $this->load->view('index');
+        $this->load->view('header and footer/footer');
+    }
+
+    public function register(){
+        $data['title'] = 'Discipulus - Registration';
+
+        $this->load->view('header and footer/header', $data);
+        $this->load->view('registration');
+        $this->load->view('header and footer/footer');
+    }
 }
---
+++ application/views/header and footer/header.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <!-- STYLES - START -->
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/bootstrap-3.3.5-dist/css/bootstrap.min.css" />
+        <link href="<?=base_url()?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="<?=base_url()?>assets/css/agency.css" rel="stylesheet">
+
+
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+        <!-- STYLES - END -->
+
+        <!-- SCRIPTS - START -->
+        <script type="text/javascript" src="<?=base_url()?>assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="<?=base_url()?>assets/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+        <!-- SCRIPTS - END -->
+
+        <!-- Plugin JavaScript -->
+        <script src="<?=base_url()?>assets/js/jquery.easing.min.js"></script>
+        <script src="<?=base_url()?>assets/js/classie.js"></script>
+        <script src="<?=base_url()?>assets/js/cbpAnimatedHeader.js"></script>
+
+        <!-- Contact Form JavaScript -->
+        <script src="<?=base_url()?>assets/js/jqBootstrapValidation.js"></script>
+        <script src="<?=base_url()?>assets/js/contact_me.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="<?=base_url()?>assets/js/agency.js"></script>
+
+        <title><?=$title?></title>
+    </head>
+    <body id="page-top" class="index">
